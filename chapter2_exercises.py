ee649839b4eafe1596da8b7c9852501a89598495
NickRog <rogov.nick@gmail.com>
2013-03-05 08:47:31 -0500

    30 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,21 @@
 # Exercises for chapter 2: Problems 2.1, 2.2, 2.3, and 2.4 in Think Python
+# Nick Rogov
 
-# Name
+# Problem 2.1: Integers entered with a leading zero are interpreted as octal (base 8), 
+# hence 02492 is interpreted an an invalid number, and 02132 as 1114 = 2*(8^3) + 1*(8^2) + 3*(8^1) + 2*(8^0)
+
+# Problem 2.2: Initial code produced no output. Modifed code with 'print' outputs 5 and 6.
+
+# Problem 2.3: 
+# 1) 8, int
+# 2) 8.5, float
+# 3) 4.0, float
+# 4) 11, int
+# 5) '.....', str
+
+# Problem 2.4:
+# 1) 523.5987755982989
+# 2) $945.45
+# 3) 7:30:6 am
+
+
